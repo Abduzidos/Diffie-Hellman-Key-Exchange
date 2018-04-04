bd79095b3282606e9693fe35aac740529c92486e
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2018-04-04 19:50:32 -0300

    Add apply_dh_formula function

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+# The main formula is A = (G**a) % P
+# To get the final key you use the same formula, with different variables S = (A**a) % P
+
+
+def apply_dh_formula(G, a, P):
+    return (G**a) % P
+
+
 def main():
+    # Variables to Test, it will be sent in the communication
     REMOTE_G = 5
     REMOTE_P = 3
+
+    # Just to test, IT WILL NOT EXIST AND NOT WILL BE SENT IN THE VERSION WITH COMMUNICATION
+    REMOTE_a = 12128109
 
     # Getting the G param
     LOCAL_G = int(input(
@@ -16,8 +28,19 @@ def main():
         LOCAL_P = int(input(
             'Choose a number between 0 to 99 a index for your P in the prime number table: '))
 
-    LOCAL_PRIVATE_KEY = int(input(
+    if(LOCAL_G != REMOTE_G):
+        print("You should come to consensus about the G number")
+        return
+
+    if(LOCAL_P != REMOTE_P):
+        print("You should come to consensus about the P number")
+        return
+
+    LOCAL_a = int(input(
         'Choose a number to be your local private key (the little a): '))
+
+    A = apply_dh_formula(LOCAL_G, LOCAL_a, LOCAL_P)
+    print(A)
 
 
 if __name__ == "__main__":
