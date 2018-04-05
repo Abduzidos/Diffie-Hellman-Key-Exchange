df52d67b8ab5f14f0df9cff43ebd9f5fd0925037
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2018-04-05 07:42:26 -0300

    Mocking values from another pc to test

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 # To get the final key you use the same formula, with different variables S = (A**a) % P
 from Cliente import *
 
+
 def apply_dh_formula(G, a, P):
     return (G**a) % P
+
 
 def conectar():
     ip = input("Digite o ip para qual se conectar: ")
@@ -11,27 +13,30 @@ def conectar():
         print("Entrada inválida")
         receberIp()
     else:
-        novo_cliente = Cliente().conectar(sPORT,ip)
+        novo_cliente = Cliente().conectar(sPORT, ip)
+
 
 def main():
-    # Variables to Test, it will be sent in the communication
-    REMOTE_G = 5
-    REMOTE_P = 3
 
-    # Just to test, IT WILL NOT EXIST AND NOT WILL BE SENT IN THE VERSION WITH COMMUNICATION
-    REMOTE_a = 12128109
+     # Just to test, IT WILL NOT EXIST AND NOT WILL BE SENT IN THE VERSION WITH COMMUNICATION
+    REMOTE_a = 121281
+
+    # Variables to Test, it will be sent in the communication
+    REMOTE_G = 179424691
+    REMOTE_P = 179426549
+    REMOTE_A = apply_dh_formula(REMOTE_G, REMOTE_a, REMOTE_P)
 
     # Getting the G param
     LOCAL_G = int(input(
         'Choose a number between 0 to 99 a index for your G in the prime number table: '))
-    while LOCAL_G not in range(0, 99):
+    while LOCAL_G not in range(0, 179424692):
         LOCAL_G = int(input(
             'Choose a number between 0 to 99 a index for your G in the prime number table: '))
 
     # Getting the P param
     LOCAL_P = int(input(
         'Choose a number between 0 to 99 a index for your P in the prime number table: '))
-    while LOCAL_P not in range(0, 99):
+    while LOCAL_P not in range(0, 179426550):
         LOCAL_P = int(input(
             'Choose a number between 0 to 99 a index for your P in the prime number table: '))
 
@@ -46,9 +51,21 @@ def main():
     LOCAL_a = int(input(
         'Choose a number to be your local private key (the little a): '))
 
+    # This A will be sent to the other pc
     A = apply_dh_formula(LOCAL_G, LOCAL_a, LOCAL_P)
+
+    # This computer Part
+    S = apply_dh_formula(REMOTE_A, LOCAL_a, LOCAL_P)
+
+    # Just to test
+    REMOTE_S = apply_dh_formula(A, REMOTE_a, REMOTE_P)
+
     print(A)
+    print(REMOTE_S)
+    print(S)
 
     conectar()
+
+
 if __name__ == "__main__":
     main()
